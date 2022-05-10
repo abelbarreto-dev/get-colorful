3219379b58d3c545ad0da0c3654eb9262e19f297
abelbcarvalho <abelbdecarvalho@gmail.com>
2022-05-10 10:47:08 -0300

    Message model and it singleton created and linked

---
+++ src/core/message/message.py
@@ -0,0 +1,57 @@
+class Message:
+    """It class is a model of data to inform user about
+    """
+
+    def __init__(self) -> None:
+        """New Message.
+
+        Attributes Created:
+            - warning: bool
+            - error: bool
+            - success: bool
+            - text: str
+
+        Methods:
+            - clear_msg() :: to clear data instance.
+        """
+        self._text = ''
+        self._warning = self._error = self._success = False
+
+    @property
+    def text(self) -> str:
+        return self._text
+
+    @text.setter
+    def text(self, text: str) -> None:
+        self._text = text
+
+    @property
+    def warning(self) -> bool:
+        return self._warning
+
+    @warning.setter
+    def warning(self, warning: bool) -> None:
+        self._warning = warning
+
+    @property
+    def error(self) -> bool:
+        return self._error
+
+    @error.setter
+    def error(self, error: bool) -> None:
+        self._error = error
+
+    @property
+    def success(self) -> bool:
+        return self._success
+
+    @success.setter
+    def success(self, success: bool) -> None:
+        self._success = success
+
+    def clear_msg(self) -> None:
+        """This method has the function only delete
+        all current data from it instance.
+        """
+        self.text = ''
+        self.warning = self.error = self.success = False
